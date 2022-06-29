bd8c48e0d0a726dbfad111e0839048af307251c7
Pawel <25pawel@gmail.com>
2022-06-29 23:56:48 +0200

    Alex Allain - chapter 3 - task 2

--- main.cpp
+++ main.cpp
@@ -3,14 +3,37 @@
 
 using namespace std;
 
+static void printGivenName(void);
+static void calcAndPrintSumOfTwoNumbers(void);
+
 int main()
+{
+    printGivenName();
+    calcAndPrintSumOfTwoNumbers();
+
+    return 0;
+}
+
+static void printGivenName(void)
 {
     string name;
 
     cout << "Enter your name: ";
     cin >> name;
 
-    cout << "Your name is: " << name;
+    cout << "Your name is: " << name << "\n";
+}
 
-    return 0;
+static void calcAndPrintSumOfTwoNumbers(void)
+{
+    int number1 = 0;
+    int number2 = 0;
+
+    cout << "Enter first number: ";
+    cin >> number1;
+
+    cout << "Enter second number: ";
+    cin >> number2;
+
+    cout << "Sum of numbers is: " << number1 + number2;
 }
